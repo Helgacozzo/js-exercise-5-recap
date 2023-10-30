69a8bcc84f1463f63134ea074d6089350ce65c15
helgacozzo <helgacozzo@gmail.com>
2023-10-30 14:46:21 +0100

    feat/secondo esercizio

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
 // 1. Crea una funzione che crea la versione girata di una parola. Chiedi all'utente una parola e mostra la versione girata della parola.
 
-function invertiParola(){
-    let parolaInvertita = ' ';
-    let  parola = prompt('scrivi una parola');
-    for(let i = parola.length - 1; i >= 0; i--){
-        parolaInvertita += parola[i];
-    }
-    alert(parolaInvertita);
-}
-invertiParola();
+// function invertiParola(){
+//     let parolaInvertita = ' ';
+//     let  parola = prompt('scrivi una parola');
+//     for(let i = parola.length - 1; i >= 0; i--){
+//         parolaInvertita += parola[i];
+//     }
+//     alert(parolaInvertita);
+// }
+// invertiParola();
+
+
+
+// 2. Crea una funzione che concatena una lista di stringhe. Chiedi all'utente quante stringhe vuole concatenare e poi mostragli il risultato.
+
+// function listaStringhe(){
+//     let stringa = ' ';
+//     let numStringhe = prompt('quante stringhe vuoi concatenare?');
+//     for(let i = 0; i < numStringhe; i++){
+//         stringheConcatenate = prompt('inserisci la stringa');
+//         stringa += stringheConcatenate;
+//     }
+//      alert(stringa);
+// }
+
+// listaStringhe();
+
+
+
+// 3. Crea una funzione che calcola il doppio di un numero. Quindi chiedi all'utente 10 numeri. Per ogni numero, mostra all'utente il doppio, usando la funzione.
